7f8dce53a4f1fa387c153ba4e882ab490d9551ff
Lucas Boucinha <lboucin@ansys.com>
2022-09-22 15:17:30 +0200

    PR comment - simplify some test_utilies module logic

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -11,12 +11,6 @@ def compare_dictionary(dict1: dict, dict2: dict):
     if dict1.keys() != dict2.keys():
         return False
     for key, value in dict1.items():
-        if type(value) is str:
-            if value != dict2[key]:
-                return False
-        if type(value) is int:
-            if value != dict2[key]:
-                return False
         if type(value) is float:
             if not compare_floats(value, dict2[key]):
                 return False
