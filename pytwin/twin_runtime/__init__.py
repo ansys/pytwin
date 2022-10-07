905f16d61eee8756fbf769cd98becaf4db754d7b
chrpetre <christophe.petre@gmail.com>
2022-10-07 17:44:10 +0200

    finalizing api/examples doc

--- pytwin/twin_runtime/__init__.py
+++ pytwin/twin_runtime/__init__.py
@@ -1,4 +1,4 @@
-from .twin_runtime_core import TwinRuntime
+from .twin_runtime_core import TwinRuntime, LogLevel
 from .twin_runtime_error import TwinRuntimeError
 
 __all__ = ['twin_runtime_core', 'twin_runtime_error', 'TwinRuntime', 'TwinRuntimeError']
