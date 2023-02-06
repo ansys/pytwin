c82de5864adcb167431f77ee2e976798af0be47c
chrpetre <108675940+chrpetre@users.noreply.github.com>
2023-02-06 15:53:22 +0100

    Fix/doc_strings (#71)

--- src/ansys/pytwin/evaluate/twin_model.py
+++ src/ansys/pytwin/evaluate/twin_model.py
@@ -376,15 +376,16 @@ class TwinModel(Model):
     def tbrom_info(self):
         """
         Return a dictionary with TBROM model names included in the Twin and their corresponding 3D visualization
-        capabilities available (e.g. snapshots, and optionally images generation). If no TBROM is included in the
-        Twin, it returns None
+        capabilities available (e.g. snapshots, and optionally images generation). If TwinModel has not been
+        initialized, or no TBROM is included in the Twin, it returns None
         """
         return self._tbrom_info
 
     @property
     def tbrom_names(self):
         """
-        Return available TBROM model names. If no TBROM is included in the Twin, it returns an empty list.
+        Return available TBROM model names. If TwinModel has not been initialized, or no TBROM is included in the
+        Twin, it returns an empty list.
         """
         if self._tbrom_info is not None:
             return list(self._tbrom_info)
