ec19b257028f31c5d2274b91624b134000c616e1
Lucas Boucinha <108674439+lboucin@users.noreply.github.com>
2023-02-23 16:22:57 +0100

    Update tests/evaluate/test_twin_model.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- tests/evaluate/test_twin_model.py
+++ tests/evaluate/test_twin_model.py
@@ -584,7 +584,7 @@ class TestTwinModel:
         try:
             twin.get_geometry_filepath(rom_name="test")
         except TwinModelError as e:
-            assert "Twin model does not include any TBROM." in str(e)
+            assert "Twin model does not include any TBROMs." in str(e)
         try:
             twin.get_snapshot_filepath(rom_name="test")
         except TwinModelError as e:
