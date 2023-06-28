fc7238b211c81aa249db1069c98f9b11af73b638
Lucas Boucinha <lboucin@ansys.com>
2023-06-28 18:37:05 +0200

    Add debug print

--- tests/evaluate/test_tbrom.py
+++ tests/evaluate/test_tbrom.py
@@ -142,6 +142,9 @@ class TestTbRom:
         assert twinmodel.tbrom_count is 1
         name = twinmodel.tbrom_names[0]
         tbrom1 = twinmodel._tbroms[name]
+        print('\n', tbrom1._infmcs)
+        print(tbrom1.field_input_names[0], len(tbrom1._infbasis[tbrom1.field_input_names[0]]))
+        print(tbrom1.field_input_names[1], len(tbrom1._infbasis[tbrom1.field_input_names[1]]))
         assert tbrom1.field_input_count is 2
         assert tbrom1._hasoutmcs is True
         assert tbrom1._hasinfmcs[tbrom1.field_input_names[0]] is True  # pressure
